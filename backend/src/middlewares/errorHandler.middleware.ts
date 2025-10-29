00fb7828a8e66f84867df60f98ec66fd9259807e
MrArnabGhosh <ghosharnab460@gmail.com>
2025-10-29 21:37:16 +0530

    Add error handling and utility classes for improved error management

---
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,46 @@
+import { ErrorRequestHandler, Response } from 'express';
+// import { ZodError } from 'zod';
+import { HTTPSTATUS } from '../config/http.config';
+import { AppError } from '../utils/app-error';
+import { logger } from '../utils/logger';
+import { ErrorCodeEnum } from '../enums/error-code.enum';
+import { error, log } from 'console';
+
+
+
+export const errorHandler: ErrorRequestHandler = (
+  error,
+  req,
+  res,
+  next,
+): any => {
+    console.log(`Error occured on PATH:${req.path}`,{
+      body: req.body,
+      params:req.params,
+      error,
+    });
+    
+    
+
+    if (error instanceof SyntaxError && 'body' in error) {
+      return res.status(HTTPSTATUS.BAD_REQUEST).json({
+        message: 'Invalid json format, please check your request body',
+      });
+
+    }
+
+
+
+    if (error instanceof AppError) {
+      return res.status(error.statusCode).json({
+        message: error.message,
+        errorCode: error.errorCode,
+
+      });
+    }
+
+    return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal Server error',
+      error: error?.message || 'Unknown error',
+    });
+  };
